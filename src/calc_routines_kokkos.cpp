71b49fcdd2f2a7ddbcd3979ac979e8c03378ae86
Lewis Russell <lewisrussell222@gmail.com>
2024-06-18 15:29:13 +0100

    Changes to View Dims, removal of old comments

--- src/calc_routines_kokkos.cpp
+++ src/calc_routines_kokkos.cpp
@@ -6,44 +6,40 @@
 #include "files.h"
 
 /* --------------------------------------------------------------------------------------------- */
-/*void calc_routines_kokkos::pbc(std::vector<std::vector<double>>& R, std::vector<double>&iR,\
-         const std::array<double, 3>& L, const int& N) 
-{
-  /*Kokkos::parallel_for(N, KOKKOS_LAMBDA(int n) {
-    for (int i = 0; i < 3; ++i) {
-      if (R(i,n) < -0.5 * L[i]) {
-        R[i][n] += L[i];
-        iR[n] -= 1;
+void calc_routines_kokkos::write_to_file_KK(std::string file_write_path, Kokkos::View<double**> \
+                  data_vec_3D, double t, int it, const int& n) {
+
+   std::ofstream file_out;
+   file_out.open(file_write_path, std::ios::app); // Opens the file in append mode
+   if (file_out.fail()) {
+      std::cout << "ERROR: Could not open or write to file!\n" << std::endl;
+   } else {
+      file_out << "Time: " << t << std::endl;
+      file_out << "Iteration Step: " << it << std::endl;
+      for (int i=0; i<n; i++) {
+         file_out << std::setw(10) << data_vec_3D(0,i) << \
+             "\t" << std::setw(10) << data_vec_3D(1,i) << \
+             "\t" << std::setw(10) << data_vec_3D(2,i) << std::endl;
       }
-      if (R[i][n] > 0.5 * L[i]) {
-        R[i][n] -= L[i];
-        iR[n] += 1;
-      }
-    }
-  });
-}*/
+   file_out.close();
+   }
+}
 
 /* --------------------------------------------------------------------------------------------- */
-Kokkos::View<double*[3]> calc_routines_kokkos::force_routine(
-  Kokkos::View<double*[3]>& R, Kokkos::View<double*[3]>& V,\
+Kokkos::View<double**> calc_routines_kokkos::force_routine(
+  const Kokkos::View<double**>& R, const Kokkos::View<double**>& V,\
   const double& m, const double& gamma, const double& KbT, const double& dt,\
   const double& energy_scale, const double& length_scale, const double& rc1,\
   const std::array<double, 3>& L, const int& N,\
-  //std::default_random_engine& random_engine, std::normal_distribution<double>& dist)
   Kokkos::Random_XorShift64_Pool<>& rand_pool)
-  //Kokkos::Random_XorShift64_Pool<>::generator_type& RNG) 
 {
-  Kokkos::View<double*[3]> force_array_KK("force_array", N);
-  //printf("Calculating forces...\n");
-  Kokkos::View<double[3]> r("r",0.0);
-  Kokkos::View<double[3]> F_lj("F_lj",0.0);
-  Kokkos::View<double[3]> F_r("F_r",0.0);
-  Kokkos::View<double[3]> eta("eta",0.0);
-  //double eta;
-
+  Kokkos::View<double**> force_array_KK("force_array", R.extent(0), R.extent(1));
   typedef typename Kokkos::Random_XorShift64_Pool<>::generator_type rand_type;
-
+  // Parallel Force Routine
   Kokkos::parallel_for(N, KOKKOS_LAMBDA(int k) {
+    double F_lj, F_r;
+    Kokkos::View<double[3]> r("r",0.0);
+    Kokkos::View<double[3]> eta("eta",0.0);
     for (int j = k; j < N; ++j) {
       // Minimum Image Convention
       for (int p = 0; p < 3; ++p) {
@@ -51,45 +47,32 @@ Kokkos::View<double*[3]> calc_routines_kokkos::force_routine(
         if (r(p) > 0.5 * L[p]) {
           r(p) -= L[p];
         }
-        if (r[p] < -0.5 * L[p]) {
+        if (r(p) < -0.5 * L[p]) {
           r(p) += L[p];
         }
       }
       double r_norm = std::sqrt(r(0) * r(0) + r(1) * r(1) + r(2) * r(2));
       rand_type RNG = rand_pool.get_state();
+      eta(0) = RNG.normal(0.0,1.0);
+      eta(1) = RNG.normal(0.0,1.0);
+      eta(2) = RNG.normal(0.0,1.0);
+      rand_pool.free_state(RNG);
       for (int p = 0; p < 3; ++p) {
         // Force Calculation : Lennard-Jones Potential
         if ((r_norm < rc1) && (j != k)) {
-          F_lj(p) = -(r(p) * (4 * energy_scale * ( \
+          F_lj = -(r(p) * (4 * energy_scale * ( \
                      ((std::pow(length_scale, 12) * (-12)) / std::pow(r_norm, 14)) + \
                      ((std::pow(length_scale, 6) * (6))   /  std::pow(r_norm, 8)) )));
-        } else F_lj(p) = 0.0;
-        //printf("F_lj(p): %f\n", F_lj(p));
+        } else F_lj = 0.0;
         // Force Calculation: Langevin Dynamics
-        //printf("Calculating random number...\n");
-        eta(p) = RNG.normal(0.0,1.0);
-        //printf("eta(p) = %f\n", eta(p));
-        //eta = dist(random_engine);
-        //double eta = 0.0;
-        F_r(p) = std::sqrt(2 * m * gamma * KbT) * eta(p) / std::sqrt(dt / 2);
-        //printf("F_r(p): %f\n", F_r(p));
-        //printf("F_r: Done\n");
+        F_r = std::sqrt(2 * m * gamma * KbT) * eta(p) / std::sqrt(dt / 2);
         // Force Calculation: Total Force
-        force_array_KK(p,k) = ( F_lj(p)/m - gamma * V(p,k) + F_r(p)/std::sqrt(m) );
-        force_array_KK(p,j) = (-F_lj(p)/m - gamma * V(p,j) + F_r(p)/std::sqrt(m) );
-        //printf("force_array_KK(p,k): %f\n", force_array_KK(p,k));
-        //printf("force_array_KK(p,j): %f\n", force_array_KK(p,j));
-        //printf("force_array_KK increment: Done\n");
+        force_array_KK(p,k) = ( F_lj/m - gamma * V(p,k) + F_r/std::sqrt(m) );
+        force_array_KK(p,j) = (-F_lj/m - gamma * V(p,j) + F_r/std::sqrt(m) );
       }
-      rand_pool.free_state(RNG);
-      //printf("End of inner loop\n");
     }
-    //printf("End of outer loop\n");
   });
-  //rand_pool.free_state(RNG);
-  //return force_array_KK;
-  //printf("End of parallel_for\n");
-
+  // End of Parallel Force Routine
   return force_array_KK;
 }
 
@@ -103,96 +86,61 @@ void calc_routines_kokkos::Verlet_Integration(
   const std::array<double, 3>& L, std::vector<double>& iR, const int& seed) 
 {
   double t_i = t;
-  std::vector<double> time;
-  
-  //std::vector<std::vector<double>> V_i_half(3, std::vector<double>(N, 0.0));
-  Kokkos::View<double*[3]> V_i_half_KK("V_i_half_KK", N);
-  
-  //std::vector<double> flat_R;
-  //for (const auto& row : R) {
-  //  flat_R.insert(flat_R.end(), row.begin(), row.end());
-  //}
-  Kokkos::View<double*[3]> R_KK("R_KK", N);
-  Kokkos::View<double*[3]> V_KK("V_KK", N);
-  //Kokkos::deep_copy(R_KK, R.data());
-  for (int n = 0; n < N; n++) {
-    for (int p = 0; p < 3; p++) {
+  // Initialising Kokkos Views
+  Kokkos::View<double**> V_i_half_KK("V_i_half_KK", V.size(), V[0].size());
+  Kokkos::View<double**> temp_force_routine("temp_force_routine", V.size(), V[0].size());
+  Kokkos::View<double**> R_KK("R_KK", R.size(), R[0].size());
+  Kokkos::View<double**> V_KK("V_KK", V.size(), V[0].size());
+  // Copying Vanilla C++ std::vectors to Kokkos Views
+  for (int p = 0; p < 3; p++) {
+    for (int n = 0; n < N; n++) {
       R_KK(p,n) = R[p][n];
       V_KK(p,n) = V[p][n];
     }
   }
-  //printf("Done R_KK and V_KK\n");
-  //std::vector<std::vector<double>> temp_force_routine(3, std::vector<double>(N, 0.0));
-  Kokkos::View<double*[3]> temp_force_routine("temp_force_routine", N);
-  //printf("Done temp_force_routine\n");
-  //std::default_random_engine random_engine(seed);
-  //std::normal_distribution<double> dist(0.0, 1.0);
-  //printf("Done random_engine and dist\n");
-
-
-  //Kokkos::View<double*[3]> temp_force_routine("temp_force_routine", N);
-  //printf("Initialising random number generator pool...\n");
+  // Defining Kokkos RNG Pool
   Kokkos::Random_XorShift64_Pool<> rand_pool(seed);
-  //typedef typename Kokkos::Random_XorShift64_Pool<>::generator_type rand_type;
-  //Kokkos::Random_XorShift64_Pool<>::generator_type RNG = rand_pool.get_state();
-  //printf("Beginning Verlet Integration...\n");
+  // Verlet Integration Loop
   for (int i = 0; i <= t_n; i++) {
-    /*for (int n = 0; n < N; n++) {
-      for (int p = 0; p < 3; p++) {
-        R_KK(p,n) = R[p][n];
-        V_KK(p,n) = V[p][n];
-      }
-    }*/
+    // 1st Force Routine Call
     temp_force_routine = force_routine(R_KK, V_KK, m, gamma, KbT, dt, \
                                        energy_scale, length_scale, rc1, L, N, \
                                        rand_pool);
-                                       //random_engine, dist);
-    //printf("IN VERLET: temp_force_routine(0,0) = %f\n", temp_force_routine(0,0));
-
+    // Parallel Verlet 1st Step
     Kokkos::parallel_for(N, KOKKOS_LAMBDA(int n) {
       for (int p = 0; p < 3; p++) {
         V_i_half_KK(p,n) = V_KK(p,n) + temp_force_routine(p,n) * dt / 2;
         R_KK(p,n) += V_i_half_KK(p,n) * dt;
       }
     });
-    
+    // 2nd Force Routine Call
     temp_force_routine = force_routine(R_KK, V_i_half_KK, m, gamma, KbT, dt, \
                                        energy_scale, length_scale, rc1, L, N, \
                                        rand_pool);
-                                       //random_engine, dist);
-
+    // Parallel Verlet 2nd Step
     Kokkos::parallel_for(N, KOKKOS_LAMBDA(int n) {
       for (int p = 0; p < 3; p++) {
         V_KK(p,n) = V_i_half_KK(p,n) + temp_force_routine(p,n) * dt / 2;
       }
     });
-    
-    //pbc(R, iR, L, N);
+    // Periodic Boundary Conditions
     for (int n = 0; n < N; ++n) {
-      for (int i = 0; i < 3; ++i) {
-        if (R_KK(i,n) < -0.5 * L[i]) {
-          printf("+=L");
-          R_KK(i,n) += L[i];
+      for (int p = 0; p < 3; ++p) {
+        if (R_KK(p,n) < -0.5 * L[p]) {
+          R_KK(p,n) += L[p];
           iR[n] -= 1;
         }
-        if (R_KK(i,n) > 0.5 * L[i]) {
-          printf("-=L");
-          R_KK(i,n) -= L[i];
+        if (R_KK(p,n) > 0.5 * L[p]) {
+          R_KK(p,n) -= L[p];
           iR[n] += 1;
         }
       }
     }
-
+    // Writing to Data Files
     if ( (i % t_pw) == 0) {
-      for (int n = 0; n < N; n++) {
-        for (int p = 0; p < 3; p++) {
-          R[p][n] = R_KK(p,n);
-          V[p][n] = V_KK(p,n);
-        }
-      }
       printf("\rWriting to data file at iteration: %d, time: %f", i, t_i);
-      write_to_file(filename_x, R, t_i, i); // store (appending) the position data to file 
-      write_to_file(filename_v, V, t_i, i); // store (appending) the velocity data to file
+      write_to_file_KK(filename_x, R_KK, t_i, i, N); // store (appending) the position data to file 
+      write_to_file_KK(filename_v, V_KK, t_i, i, N); // store (appending) the velocity data to file
     }
     timesteps[i] = t_i; // stores time values
     t_i += dt;
--- src/calc_routines_kokkos.h
+++ src/calc_routines_kokkos.h
@@ -7,35 +7,21 @@
 #include <Kokkos_Core.hpp>
 #include <Kokkos_Random.hpp>
 
-//template<class Device>
 class calc_routines_kokkos : public calc_routines {
 public:
 
    calc_routines_kokkos(){};  // Empty constructor
    ~calc_routines_kokkos(){}; // Empty destructor
 
-   //void pbc(std::vector<std::vector<double>>&, std::vector<double>&,\
-   //       const std::array<double, 3>&, const int&);
+   void write_to_file_KK(std::string file_write_path, const Kokkos::View<double**>,\
+                     double, int, const int&);
 
-   /*void initialise_positions(std::vector<std::vector<double>>&,\
-                       const int&, const std::array<double, 3>&, const double&);
-
-   void initialise_velocities(std::vector<std::vector<double>>&,\
-                        const int&, const std::array<double, 3>&,\
-                        const double&, const double&, const int&);*/
-
-   /*void pbc(Kokkos::View<double*[3]> &, std::vector<double>&,\
-          const std::array<double, 3>&, const int&);*/
-
-   Kokkos::View<double*[3]> force_routine(Kokkos::View<double*[3]>&,\
-                     Kokkos::View<double*[3]>&,\
+   Kokkos::View<double**> force_routine(const Kokkos::View<double**>&,\
+                     const Kokkos::View<double**>&,\
                      const double&, const double&, const double&,\
                      const double&, const double&, const double&,\
                      const double&, const std::array<double, 3>&, const int&,\
-                     //std::default_random_engine&,\
-                     //std::normal_distribution<double>&);
                      Kokkos::Random_XorShift64_Pool<>&);
-                     //Kokkos::Random_XorShift64_Pool<>::generator_type&);
 
    void Verlet_Integration(std::vector<std::vector<double>>, std::vector<std::vector<double>>,\
                      const double&, const int&, const double& dt, const double& t,\
